101a35f087be28d9cfcbb8c2d056bce821e13fbf
Jeong-Yeon-Lee <0906ljy@gmail.com>
2023-02-13 15:25:43 +0900

    firestore 세팅 및 테스트용 샘플 데이터 만들어두기

--- birders-app/src/pages/BoardPage.js
+++ birders-app/src/pages/BoardPage.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// 파이어베이서 파일에서 import 해온 db
+import { db } from "../firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function BoardPage() {
+  const [posts, setPosts] = useState([]);
+  //const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const q = collection(db, "posts");
+      const dbPosts = await getDocs(q);
+      //const data = await getDocs(postsCollectionRef);
+      console.log("dbpost", dbPosts);
+      dbPosts.forEach((doc) => {
+        const postObject = {
+          ...doc.data(),
+          id: doc.id,
+        };
+        //setPosts((prev) => [postObject, ...prev]);
+        setPosts((prev) => [postObject]);
+      });
+    };
+
+    getPosts();
+  }, []);
+
+  console.log(posts);
   return <div>BoardPage</div>;
 }
--- birders-app/src/pages/EditorPage.js
+++ birders-app/src/pages/EditorPage.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
 
 export default function EditorPage() {
-  return <div>EditorPage</div>;
+  const [num, setNum] = useState(0);
+
+  const comment = (num) => {
+    return {
+      commentId: `commentId${num}`,
+      userDisplayName: `userDisplayName${num}`,
+      textContents: `textContents${num}`,
+      userId: `userId${num}`,
+    };
+  };
+  const [post, setPost] = useState({
+    title: `테스트 샘플타이틀${num}`,
+    textContents: `샘플 컨텐츠${num}`,
+    createdAt: Date.now(),
+    userId: `sampleuser${num}`,
+    userDisplayName: `샘플 유저${num}`,
+    editedAt: Date.now(),
+    comments: comment(num),
+    birdList: ["참새", "까치"],
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await addDoc(collection(db, "posts"), {
+      post,
+    });
+    setNum(num + 1);
+    setPost({
+      ...post,
+      title: `테스트 샘플타이틀${num}`,
+      textContents: `샘플 컨텐츠${num}`,
+      createdAt: Date.now(),
+      userId: `sampleuser${num}`,
+      userDisplayName: `샘플 유저${num}`,
+      editedAt: Date.now(),
+      comments: comment(num),
+      birdList: ["참새", "까치"],
+    });
+    console.log(post);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="submit" value="테스트" />
+    </form>
+  );
 }
